e093373358bc7247a917de5333f2ac3bc72dc50e
Sarthak Negi <46480857+Scartak@users.noreply.github.com>
2026-04-02 15:04:56 +1300

    Started frequency analyzer

--- Assignment_1/software/project1/obj/default/hello_world.d
+++ Assignment_1/software/project1/obj/default/hello_world.d
@@ -1,1 +1,24 @@
-obj/default/hello_world.o: hello_world.c
+obj/default/hello_world.o: hello_world.c ../project1_bsp/system.h \
+ ../project1_bsp/linker.h ../project1_bsp//HAL/inc/sys/alt_irq.h \
+ ../project1_bsp//HAL/inc/nios2.h ../project1_bsp//HAL/inc/alt_types.h \
+ ../project1_bsp//HAL/inc/priv/alt_legacy_irq.h \
+ ../project1_bsp//HAL/inc/io.h ../project1_bsp//HAL/inc/alt_types.h \
+ ../project1_bsp//drivers/inc/altera_avalon_pio_regs.h
+
+../project1_bsp/system.h:
+
+../project1_bsp/linker.h:
+
+../project1_bsp//HAL/inc/sys/alt_irq.h:
+
+../project1_bsp//HAL/inc/nios2.h:
+
+../project1_bsp//HAL/inc/alt_types.h:
+
+../project1_bsp//HAL/inc/priv/alt_legacy_irq.h:
+
+../project1_bsp//HAL/inc/io.h:
+
+../project1_bsp//HAL/inc/alt_types.h:
+
+../project1_bsp//drivers/inc/altera_avalon_pio_regs.h:
