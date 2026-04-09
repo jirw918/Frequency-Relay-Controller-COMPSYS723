df8e5ef791e71785d332ec56cddfc993187d7dd7
Sarthak Negi <46480857+Scartak@users.noreply.github.com>
2026-04-09 16:08:31 +1200

    issue with bsp

--- Assignment_1/software/project1/obj/default/hello_world.d
+++ Assignment_1/software/project1/obj/default/hello_world.d
@@ -3,7 +3,11 @@ obj/default/hello_world.o: hello_world.c ../project1_bsp/system.h \
  ../project1_bsp//HAL/inc/nios2.h ../project1_bsp//HAL/inc/alt_types.h \
  ../project1_bsp//HAL/inc/priv/alt_legacy_irq.h \
  ../project1_bsp//HAL/inc/io.h ../project1_bsp//HAL/inc/alt_types.h \
- ../project1_bsp//drivers/inc/altera_avalon_pio_regs.h
+ ../project1_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ freertos/FreeRTOS.h freertos/FreeRTOSConfig.h freertos/projdefs.h \
+ freertos/portable.h freertos/deprecated_definitions.h \
+ freertos/portmacro.h freertos/mpu_wrappers.h freertos/queue.h \
+ freertos/task.h freertos/list.h freertos/timers.h freertos/task.h
 
 ../project1_bsp/system.h:
 
@@ -22,3 +26,27 @@ obj/default/hello_world.o: hello_world.c ../project1_bsp/system.h \
 ../project1_bsp//HAL/inc/alt_types.h:
 
 ../project1_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+freertos/FreeRTOS.h:
+
+freertos/FreeRTOSConfig.h:
+
+freertos/projdefs.h:
+
+freertos/portable.h:
+
+freertos/deprecated_definitions.h:
+
+freertos/portmacro.h:
+
+freertos/mpu_wrappers.h:
+
+freertos/queue.h:
+
+freertos/task.h:
+
+freertos/list.h:
+
+freertos/timers.h:
+
+freertos/task.h:
