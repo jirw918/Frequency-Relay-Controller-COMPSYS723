792e5596ae6191f52458520a90d86239c029b088
Webpleb11 <75925526+Webpleb11@users.noreply.github.com>
2026-04-14 01:21:54 +1200

    Got to VGA

--- Freq_Relay_Controller/software/freq_relay_controller/obj/default/main.d
+++ Freq_Relay_Controller/software/freq_relay_controller/obj/default/main.d
@@ -1,20 +1,30 @@
-obj/default/main.o: main.c freertos/FreeRTOS.h freertos/FreeRTOSConfig.h \
- ../freq_relay_controller_bsp/system.h \
- ../freq_relay_controller_bsp/linker.h freertos/projdefs.h \
- freertos/portable.h freertos/deprecated_definitions.h \
- freertos/portmacro.h ../freq_relay_controller_bsp//HAL/inc/sys/alt_irq.h \
+obj/default/main.o: main.c ../freq_relay_controller_bsp/system.h \
+ ../freq_relay_controller_bsp/linker.h freertos/FreeRTOS.h \
+ freertos/FreeRTOSConfig.h freertos/projdefs.h freertos/portable.h \
+ freertos/deprecated_definitions.h freertos/portmacro.h \
+ ../freq_relay_controller_bsp//HAL/inc/sys/alt_irq.h \
  ../freq_relay_controller_bsp//HAL/inc/nios2.h \
  ../freq_relay_controller_bsp//HAL/inc/alt_types.h \
  ../freq_relay_controller_bsp//HAL/inc/priv/alt_legacy_irq.h \
- freertos/mpu_wrappers.h freertos/task.h freertos/list.h freertos/queue.h
-
-freertos/FreeRTOS.h:
-
-freertos/FreeRTOSConfig.h:
+ freertos/mpu_wrappers.h freertos/task.h freertos/list.h freertos/queue.h \
+ freertos/semphr.h freertos/queue.h freertos/portmacro.h \
+ ../freq_relay_controller_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../freq_relay_controller_bsp//HAL/inc/io.h \
+ ../freq_relay_controller_bsp//HAL/inc/alt_types.h \
+ ../freq_relay_controller_bsp//drivers/inc/altera_up_avalon_video_character_buffer_with_dma.h \
+ ../freq_relay_controller_bsp//HAL/inc/sys/alt_dev.h \
+ ../freq_relay_controller_bsp//HAL/inc/sys/alt_llist.h \
+ ../freq_relay_controller_bsp//HAL/inc/priv/alt_dev_llist.h \
+ ../freq_relay_controller_bsp//drivers/inc/altera_up_avalon_video_pixel_buffer_dma.h \
+ ../freq_relay_controller_bsp//drivers/inc/altera_up_avalon_ps2.h
 
 ../freq_relay_controller_bsp/system.h:
 
 ../freq_relay_controller_bsp/linker.h:
+
+freertos/FreeRTOS.h:
+
+freertos/FreeRTOSConfig.h:
 
 freertos/projdefs.h:
 
@@ -39,3 +49,27 @@ freertos/task.h:
 freertos/list.h:
 
 freertos/queue.h:
+
+freertos/semphr.h:
+
+freertos/queue.h:
+
+freertos/portmacro.h:
+
+../freq_relay_controller_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../freq_relay_controller_bsp//HAL/inc/io.h:
+
+../freq_relay_controller_bsp//HAL/inc/alt_types.h:
+
+../freq_relay_controller_bsp//drivers/inc/altera_up_avalon_video_character_buffer_with_dma.h:
+
+../freq_relay_controller_bsp//HAL/inc/sys/alt_dev.h:
+
+../freq_relay_controller_bsp//HAL/inc/sys/alt_llist.h:
+
+../freq_relay_controller_bsp//HAL/inc/priv/alt_dev_llist.h:
+
+../freq_relay_controller_bsp//drivers/inc/altera_up_avalon_video_pixel_buffer_dma.h:
+
+../freq_relay_controller_bsp//drivers/inc/altera_up_avalon_ps2.h:
