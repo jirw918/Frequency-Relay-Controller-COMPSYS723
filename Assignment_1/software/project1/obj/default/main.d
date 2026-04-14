ba762b810ac9e5756f2513d20527dc1a9b8fc807
Sarthak Negi <46480857+Scartak@users.noreply.github.com>
2026-04-14 17:58:02 +1200

    Mutex for frequency

--- Assignment_1/software/project1/obj/default/main.d
+++ Assignment_1/software/project1/obj/default/main.d
@@ -5,8 +5,8 @@ obj/default/main.o: main.c ../project1_bsp/system.h \
  ../project1_bsp//HAL/inc/sys/alt_irq.h ../project1_bsp//HAL/inc/nios2.h \
  ../project1_bsp//HAL/inc/alt_types.h \
  ../project1_bsp//HAL/inc/priv/alt_legacy_irq.h FreeRTOS/mpu_wrappers.h \
- FreeRTOS/task.h FreeRTOS/list.h FreeRTOS/queue.h \
- ../project1_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ FreeRTOS/task.h FreeRTOS/list.h FreeRTOS/queue.h FreeRTOS/semphr.h \
+ FreeRTOS/queue.h ../project1_bsp//drivers/inc/altera_avalon_pio_regs.h \
  ../project1_bsp//HAL/inc/io.h ../project1_bsp//HAL/inc/alt_types.h
 
 ../project1_bsp/system.h:
@@ -38,6 +38,10 @@ FreeRTOS/mpu_wrappers.h:
 FreeRTOS/task.h:
 
 FreeRTOS/list.h:
+
+FreeRTOS/queue.h:
+
+FreeRTOS/semphr.h:
 
 FreeRTOS/queue.h:
 
